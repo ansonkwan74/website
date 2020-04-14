aec9d8d086ffd4f058181955543c5957af901fa5
ansonkwan74 <59356349+ansonkwan74@users.noreply.github.com>
2020-04-14 22:07:52 +1200

    various

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -9,14 +9,14 @@ const introduction = () => {
       <div className="introInformationContainer">
         <div className="introText">
           <p>
-            I am a second year Software Engineering student at The University of
-            Auckland, and a web development enthusiast. Currently spending time
-            on sharpening my data structure and algorithms knowledge.
+            A web development enthusiast, explorer of frameworks and design.
+            Believer of JavaScript being a real programming language. Currently
+            spending time on sharpening my data structures and algorithms
+            knowledge.
           </p>
         </div>
         <div className="introButtonContainer">
           <button className="introButton">Wanna Chat?</button>
-          <button className="introButton">CV</button>
         </div>
       </div>
     </div>

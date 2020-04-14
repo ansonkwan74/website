67e7db9e5adb185d0c83a10f7d6863b1abdd8966
ansonkwan74 <59356349+ansonkwan74@users.noreply.github.com>
2020-04-15 02:23:14 +1200

    Revert "installed bootstrap"
    This reverts commit d263498d44b2fe5cf5b06318f35550f87c418af9.

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,31 +1,25 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+
 const introduction = () => {
   return (
-    <Container>
-      <Col>
-        <Row>
-          <p className="introTitle">The name is</p>
-        </Row>
-        <Row>
-          <p className="introName">Anson Kwan.</p>
-        </Row>
-        <Row>
-          <p className="introDescription">I like coding things.</p>
-        </Row>
-        <Row>
-          <p className="introText">
+    <div className="sectionWrapper">
+      <div className="introTitle">The name is</div>
+      <div className="introName">Anson Kwan.</div>
+      <div className="introDescription">I like complicated things.</div>
+      <div className="introInformationContainer">
+        <div className="introText">
+          <p>
             A web development enthusiast, explorer of frameworks and design.
             Believer of JavaScript being a real programming language. Currently
             spending time on sharpening my data structures and algorithms
             knowledge.
           </p>
-          <div className="introButtonContainer">
-            <button className="introButton">Wanna Chat?</button>
-          </div>
-        </Row>
-      </Col>
-    </Container>
+        </div>
+        <div className="introButtonContainer">
+          <button className="introButton">Wanna Chat?</button>
+        </div>
+      </div>
+    </div>
   );
 };
 

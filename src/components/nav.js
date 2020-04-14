76200c2a43fae2b9ea85aeeeb5ec5ad71a35da68
ansonkwan74 <59356349+ansonkwan74@users.noreply.github.com>
2020-04-15 02:23:09 +1200

    Revert "update"
    This reverts commit 5aea16e7a04a5606bd62036bf382da62a1a2d513.

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,15 @@ import React from "react";
 import "../styles/stylesheet.css";
 
 const nav = () => {
-  return <div className="navWrapper">nav</div>;
+  return (
+    <div className="navWrapper">
+      <button className="navButton">Anson Kwan</button>
+      <button className="navButton">About Me</button>
+      <button className="navButton">My Projects</button>
+      <button className="navButton">Contact Me</button>
+      <button className="navButton">CV</button>
+    </div>
+  );
 };
 
 export default nav;

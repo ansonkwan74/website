64a7cf3e48278f15af2b01056b1e9d288142c2e1
ansonkwan74 <59356349+ansonkwan74@users.noreply.github.com>
2020-04-16 02:30:37 +1200

    went back to before nav was deleted

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,38 +1,8 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const projects = () => {
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <Row className="introTitle" p={0}>
-            The name is
-          </Row>
-          <Row className="introName" p={0}>
-            Anson Kwan.
-          </Row>
-          <Row className="introDescription" p={0}>
-            I like coding things.
-          </Row>
-        </Col>
-      </Row>
-      <Row lg={2} sm={1}>
-        <Col className="introText">
-          A web development enthusiast, explorer of frameworks and design.
-          Believer of JavaScript being a real programming language. Currently
-          spending time on sharpening my data structures and algorithms
-          knowledge.
-        </Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col className="introButtonContainer">
-          <button className="introButton">Wanna Chat?</button>
-        </Col>
-      </Row>
-    </Container>
-  );
+  return <Container></Container>;
 };
 
 export default projects;

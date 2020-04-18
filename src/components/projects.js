ec69650dfe45277ee78217dca8c6af6ac40edd3b
ansonkwan74 <59356349+ansonkwan74@users.noreply.github.com>
2020-04-19 05:17:36 +1200

    update projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,19 +10,24 @@ const projects = () => {
           <img
             className="projectImage"
             alt="placeholder"
-            src="https://images.unsplash.com/photo-1556924784-f02bd5b5b094?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            src="https://images.unsplash.com/photo-1566849571098-b2e9eeaee53f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
           />
         </Col>
       </Row>
       <Row className="projectInfoContainer">
-        <Col xs={{ span: 6, offset: 6 }} className="projectInfoWrap">
-          <Row className="projectTitle ">Project Title</Row>
-          <Row className="projectDescription ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit.
+        <Col xs={{ span: 7, offset: 5 }} className="">
+          <Row className="projectTitle">
+            <p>
+              <span className="red-highlight">01</span> Project Title
+            </p>
+          </Row>
+          <Row>
+            <p className="projectDescription colour2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
           </Row>
           <Row className="projectTech ">React JavaScript HTML CSS Firebase</Row>
           <Row className="projectLinks ">tech</Row>
